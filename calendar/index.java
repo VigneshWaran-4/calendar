582de344897a049d576b672026c205c2a56d7b25
VigneshWaran-4 <mvikey2002@gmail.com>
2024-01-19 16:45:50 +0530

    first commit

---
+++ calendar/index.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class demo
+{
+    public ststic void main(String s[])
+    {
+        int n;
+        Scanner sc.new Scanner(System.in);
+        System.out.println("enter n value");
+        n=Sc.nextInt();
+    }
+}
